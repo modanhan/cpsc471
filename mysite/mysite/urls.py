284f4ddf06d8a722939bdf498b611569e2190022
modanhan <modanhan@live.com>
2018-04-06 21:05:08 -0600

    some login stuff

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
