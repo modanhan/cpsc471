9f861bf78af378e1d6366bc44a0a415e452489f7
modanhan <modanhan@live.com>
2018-04-08 14:42:04 -0600

    challenge detail

--- mysite/challenge/urls.py
+++ mysite/challenge/urls.py
@@ -6,4 +6,5 @@ app_name='challenge'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:challenge_id>/', views.detail, name='detail'),
 ]
--- mysite/challenge/views.py
+++ mysite/challenge/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Challenge
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
     challenges = Challenge.objects.all()
-    s = "Challenges\n"
-    for c in challenges:
-        s+=c.__str__()+"\n"
-    return HttpResponse(s)
+    context={
+        'challenges':challenges,
+    }
+    return render(request,'challenge/index.html',context)
+
+def detail(request, challenge_id):
+    challenge = get_object_or_404(Challenge, pk=challenge_id)
+    context={
+        'challenge':challenge,
+    }
+    return render(request,'challenge/detail.html',context)
