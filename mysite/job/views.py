af9cb3ab1d83f0a90479d3f1d1f1c7f1527acc5a
modanhan <modanhan@live.com>
2018-04-11 21:52:34 -0600

    added a self qualification thing

--- mysite/job/views.py
+++ mysite/job/views.py
@@ -14,6 +14,26 @@ def index(request):
     }
     return render(request,'job/index.html',context)
 
+def hasRequirement(user, tss,lss):
+    good = True
+    trs=user.topicrating_set.all()
+    lrs=user.languagerating_set.all()
+    for ts in tss:
+        if not trs.filter(topic_id=ts.topic_id).exists():
+            good=False
+            break
+        elif trs.get(topic_id=ts.topic_id).rating < ts.rating:
+            good=False
+            break
+    for ls in lss:
+        if not lrs.filter(language_id=ls.language_id).exists():
+            good=False
+            break
+        elif lrs.get(language_id=ls.language_id).rating < ls.rating:
+            good=False
+            break
+    return good
+
 def detail(request, job_id):
     job=Job.objects.get(pk=job_id)
     tss=job.jobtopicskill_set.all()
@@ -30,30 +50,18 @@ def detail(request, job_id):
     users=User.objects.all()
     userviews=[]
     for user in users:
-        good = True
-        trs=user.topicrating_set.all()
-        lrs=user.languagerating_set.all()
-        for ts in tss:
-            if not trs.filter(topic_id=ts.topic_id).exists():
-                good=False
-                break
-            elif trs.get(topic_id=ts.topic_id).rating < ts.rating:
-                good=False
-                break
-        for ls in lss:
-            if not lrs.filter(language_id=ls.language_id).exists():
-                good=False
-                break
-            elif lrs.get(language_id=ls.language_id).rating < ls.rating:
-                good=False
-                break
-        if good:
+        if hasRequirement(user,tss,lss):
             userviews.append(user)
+
+    qs='Log in to find out if you are qualified for this position!'
+    if not request.user == None:
+        qs= 'You are qualified!' if hasRequirement(request.user,tss,lss) else 'You are not qualified.'
 
     context={
         'job':job,
         'tss':tssview,
         'lss':lssview,
         'users':userviews,
+        'qs':qs,
     }
     return render(request, 'job/detail.html',context)
